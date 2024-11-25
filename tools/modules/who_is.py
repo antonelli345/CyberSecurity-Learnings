98ec0437743ac9f210d428fa62111aa9a76e7cfe
antonelli345 <fabioantonellii2016@gmail.com>
2024-11-25 20:32:19 -0300

    Improved features: Output file of whois including date

--- tools/modules/who_is.py
+++ tools/modules/who_is.py
@@ -1,4 +1,5 @@
 import whois
+from datetime import date
 
 def whoisresolver():
     print("Enter the domain:")
@@ -6,10 +7,11 @@ def whoisresolver():
     print("1 - No need output in a file")
     print("2 - Need output in a file")
     option = input() # Receives the user's choice
+    dateToday = date.strftime(date.today(), '%d-%m-%y')
     if(option)  == '1':
         print(domain.text)
     elif(option) == '2':
-        with open('whois.txt', 'w', encoding="UTF-8", errors="replace") as file:
+        with open('whois-'+ dateToday +'.txt', 'w', encoding="UTF-8", errors="replace") as file:
             file.write(domain.text) # Write the output to the file
     else:
         print("Invalid option")
