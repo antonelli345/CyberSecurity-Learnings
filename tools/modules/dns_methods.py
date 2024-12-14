abeb86a9e7de30979056e9d2578407e6514d925e
antonelli345 <fabioantonellii2016@gmail.com>
2024-12-13 21:36:27 -0300

    In this commit was fixed the bugs on call options of the implementation CLI

--- tools/modules/dns_methods.py
+++ tools/modules/dns_methods.py
@@ -1,24 +1,31 @@
 import socket
 import dns.resolver  
-# This methods resolves subdomains and get IPs with treatmenting for possible errors   
-def resolve_subdomains(domain: str, file: bool = False): # Receives the domain as a parameter
-    # Read the dns_dict1.txt file with the subdomains
-    with open('/tools/worldlist/dns_dict1.txt', 'r') as subdomain_file:
-        dictionary = subdomain_file.readlines()
-        for line in dictionary: 
-            full_domain = line.strip() + '.' + domain
-            try:
-                print(full_domain + ': ' + socket.gethostbyname(full_domain))
-            except socket.gaierror:
-                pass
+import os
+
+def resolve_subdomains(domain: str, file: bool = False):
+    # Path to the wordlist file 
+    current_dir = os.path.dirname(os.path.abspath(__file__))  # path of script
+    wordlist_path = os.path.join(current_dir, "../worldlist/dns_dict1.txt")  # relative path to wordlist
+
+    try:
+        with open(wordlist_path, "r") as subdomain_file:
+            dictionary = subdomain_file.readlines()
+
+        for line in dictionary:
+            full_domain = line.strip() + "." + domain
+            print(f"Resolved: {full_domain}")
+
+    except FileNotFoundError:
+        print(f"Error: Wordlist file not found at {wordlist_path}")
+        raise            
             
 def query_dns_records(domain: str, file: bool = False):
-    # Read the dns_dict2.txt file with the DNS records
-    records = open('/tools/wordlist/dns_dict2.txt', 'r').read().splitlines()
+    # path to the wordlist file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    wordlist_path = os.path.join(current_dir, "../worldlist/dns_dict2.txt")
+    records = open(wordlist_path, 'r').read().splitlines()
     resolver = dns.resolver.Resolver()
     resolver.nameservers = ['8.8.8.8', '8.8.4.4', '1.1.1.1', '1.0.0.1']
-    
-    # This loop iterates over each record in the file with treatmenting for possible errors
     for record in records:
         try:
             answers = resolver.resolve(domain, record)
