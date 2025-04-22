4697aee1aa75c3199bf2abea53484907c38099ef
antonelli345 <fabioantonellii2016@gmail.com>
2025-04-22 01:34:18 -0300

    Added feature shodan search with filters country city port and keyword

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -2,4 +2,5 @@ from modules.utils.export import save_to_txt
 
 __all__ = [
     "save_to_txt",
+    "save_to_csv",
 ]
--- modules/utils/export.py
+++ modules/utils/export.py
@@ -12,3 +12,22 @@ def save_to_txt(domain, output):
         typer.echo(f"\n[✔] Output saved to {output_path}")
     except Exception as e:
         typer.echo(f"\n[!] Error while saving the file: {e}")
+
+
+def save_to_csv(results, country, city, port, keyword):
+    output_dir = "./out"
+    output_path = os.path.join(output_dir, "shodan_results.csv")
+    os.makedirs(output_dir, exist_ok=True)
+    try:
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.write("IP,Port,Organization\n")
+            # Assuming results is a list of dictionaries with keys 'ip',
+            # 'port', and 'org'
+            for result in results['matches']:
+                ip = result.get('ip_str', 'N/A')
+                port = result.get('port', 'N/A')
+                org = result.get('org', 'N/A')
+                f.write(f"{ip},{port},{org}\n")
+        typer.echo(f"\n[✔] Output saved to {output_path}")
+    except Exception as e:
+        typer.echo(f"\n[!] Error while saving the file: {e}")
